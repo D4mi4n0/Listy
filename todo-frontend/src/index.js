22aeaca39a58cfe84292a05ea3a1d3078127c021
Damiano Montanaro <damiano.montanaro@studenti.unicam.it>
2025-04-08 16:05:52 +0200

    Commenti di index.js

--- todo-frontend/src/index.js
+++ todo-frontend/src/index.js
@@ -1,9 +1,15 @@
+// Importa il modulo React dalla libreria 'react'
 import React from 'react';
+// Importa il modulo ReactDOM dalla libreria 'react-dom/client'
 import ReactDOM from 'react-dom/client';
+// Importa il file CSS per gli stili
 import './index.css';
+// Importa il componente principale dell'applicazione
 import App from './App';
 
+// Crea una radice ReactDOM collegata all'elemento con id 'root' nel DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Esegue il rendering del componente App all'interno della modalità Strict di React
 root.render(
   <React.StrictMode>
     <App />
