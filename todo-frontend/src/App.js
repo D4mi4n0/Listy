c44bab85d929003cbef21c11dce07b84a8a84bf0
Damiano Montanaro <damiano.montanaro@studenti.unicam.it>
2025-04-08 16:02:25 +0200

    Commenti di App.js

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -5,13 +5,22 @@ import Dashboard from "./pages/Dashboard";
 import ConfirmDeleteAccount from "./pages/ConfirmDeleteAccount";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+// Funzione principale dell'applicazione
 function App() {
   return (
+    // Configurazione del Router per la gestione delle rotte
     <Router>
       <Routes>
+        {/* Rotta principale che reindirizza alla pagina di login */}
         <Route path="/" element={<Navigate to="/login" />} />
+        
+        {/* Rotta per la pagina di login */}
         <Route path="/login" element={<Login />} />
+        
+        {/* Rotta per la pagina di registrazione */}
         <Route path="/register" element={<Register />} />
+        
+        {/* Rotta protetta per la dashboard, accessibile solo se autenticati */}
         <Route
           path="/dashboard"
           element={
@@ -20,6 +29,8 @@ function App() {
             </ProtectedRoute>
           }
         />
+        
+        {/* Rotta protetta per la conferma di cancellazione dell'account, accessibile solo se autenticati */}
         <Route
           path="/confirm-delete-account"
           element={
@@ -33,4 +44,5 @@ function App() {
   );
 }
 
+// Esportazione del componente App come default
 export default App;
