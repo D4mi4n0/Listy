b98c21700a43b11fc0c1deecbb5ba5dd470d4086
Damiano Montanaro <damiano.montanaro@studenti.unicam.it>
2025-04-08 16:30:33 +0200

    Commenti di index.js (mobile)

--- todo-mobile/index.js
+++ todo-mobile/index.js
@@ -3,6 +3,8 @@ import { registerRootComponent } from 'expo';
 import { enableScreens } from 'react-native-screens';
 import App from './src/App';
 
+// Abilita l'uso delle schermate native per migliorare le prestazioni
 enableScreens();
 
+// Registra il componente principale dell'app in modo che venga eseguito quando l'app si avvia
 registerRootComponent(App);
