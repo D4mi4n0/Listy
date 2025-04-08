ae685af8f0817303f85f8ada7fb0af9707a68a72
Damiano Montanaro <damiano.montanaro@studenti.unicam.it>
2025-04-08 15:49:17 +0200

    Commenti di ConfirmDeleteAccount.js

--- todo-frontend/src/pages/ConfirmDeleteAccount.js
+++ todo-frontend/src/pages/ConfirmDeleteAccount.js
@@ -3,28 +3,39 @@ import api from "../services/api";
 import { useNavigate } from "react-router-dom";
 import sadGif from "../assets/addio-amici.gif";
 
+// Componente per la conferma dell'eliminazione dell'account
 const ConfirmDeleteAccount = () => {
+  // Stato per il testo di conferma inserito dall'utente
   const [confirmationText, setConfirmationText] = useState("");
   const navigate = useNavigate();
 
+  // Effetto per cambiare il titolo del documento quando il componente viene montato
   useEffect(() => {
     document.title = "Elimina Account - Listy";
   }, []);
 
+  // Funzione per gestire l'eliminazione dell'account
   const handleDeleteAccount = async () => {
+    // Controlla se il testo di conferma è corretto
     if (confirmationText === "Non ho nulla da fare") {
       try {
+        // Richiesta API per eliminare l'account
         await api.delete("/auth/delete-account");
+        // Rimuove il token dal localStorage
         localStorage.removeItem("token");
+        // Reindirizza l'utente alla pagina di registrazione
         navigate("/register");
       } catch (error) {
+        // Mostra un messaggio di errore se la richiesta fallisce
         alert("Errore nell'eliminazione dell'account: " + error.response.data.message);
       }
     } else {
+      // Mostra un messaggio di errore se il testo di conferma non è corretto
       alert("La frase di conferma non è corretta.");
     }
   };
 
+  // Funzione per tornare alla dashboard
   const goBackToDashboard = () => {
     navigate("/dashboard");
   };
@@ -37,7 +48,7 @@ const ConfirmDeleteAccount = () => {
       <div className="separator"></div>
       <div className="form-container">
         <h2>Noooo... Mi lasci così? :'( </h2>
-        <p>Sob, sob... A quanto pare non eravamo fatti per stare insieme. Però se proprio devi... Per confermare la cancellazione del tuo account, scrivi la frase "Non ho nulla da fare" e clicca sul pulsante "Elimina Account".</p>
+        <p>Sob, sob... A quanto pare non eravamo fatti per stare insieme. Però se proprio devi... Per confermare la cancellazione del tuo account, scrivi la frase "Non ho nulla da fare" e clicca [...]
         <input
           type="text"
           placeholder="Scrivi qui la frase di conferma"
