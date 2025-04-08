3e9f319e801d762e59697525eb4e338fbc1e795c
Damiano Montanaro <damiano.montanaro@studenti.unicam.it>
2025-04-08 18:22:33 +0200

    Sistemazione commenti in todo_list.sql

--- todo_list.sql
+++ todo_list.sql
@@ -1,11 +1,10 @@
 CREATE DATABASE IF NOT EXISTS todo_list;
---crea il database todo_list se non esiste già
 
 USE todo_list;
 
 CREATE TABLE IF NOT EXISTS Users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    --id univoco per ogni utente, viene automaticamente incrementato
+    /*id univoco per ogni utente, viene automaticamente incrementato*/
 
     name VARCHAR(50) NOT NULL,
 
@@ -16,7 +15,7 @@ CREATE TABLE IF NOT EXISTS Users (
 
 CREATE TABLE IF NOT EXISTS Tasks (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    --id univoco per ogni singola task, viene automaticamente incrementato
+    /*id univoco per ogni singola task, viene automaticamente incrementato*/
 
     user_id INT NOT NULL,
 
@@ -25,7 +24,14 @@ CREATE TABLE IF NOT EXISTS Tasks (
     completed BOOLEAN DEFAULT FALSE,
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    --la data e l'ora del task vengono impostate in automatico con CURRENT_TIMESTAMP
+    /*la data e l'ora del task vengono impostate in automatico con CURRENT_TIMESTAMP*/
+
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+    /*foreign key che mette in relazione il task con l'utente, con gli attributi "id" di User 
+    e "userd_id" di Task se l'utente viene eliminato si cancelleranno anche i suoi task grazie 
+    a ON DELETE CASCADE */
+);
+
 
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
     --foreign key che mette in relazione il task con l'utente, con gli attributi "id" di User e "userd_id" di Task
